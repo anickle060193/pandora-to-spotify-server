4040db1b35210ff01f7706a070723bb5f30598e9
Adam Nickle <anickle060193@gmail.com>
2017-08-05 20:52:52 -0500

    Finished routes.

--- src/routes/stationLikes.ts
+++ src/routes/stationLikes.ts
@@ -1,26 +1,74 @@
 import { Router } from 'express';
 import * as url from 'url';
 import * as rp from 'request-promise';
+import * as cheerio from 'cheerio';
 
 const stationsLikes = Router();
 
-stationsLikes.get( '/:stationId', ( request, response, next ) =>
+stationsLikes.get( '/:stationId', async ( request, response, next ) =>
 {
-    console.log( 'Station Likes: ' + request.params.stationId );
+    let stationId = request.params.stationId;
+    console.log( 'Station Likes: ' + stationId );
 
-    let oldUrl = url.parse( request.url, true );
-    let newUrl = url.format( { protocol: 'https', host: 'www.pandora.com', pathname: '/content/station_track_thumbs/', search: oldUrl.search } );
-    return rp( newUrl )
-        .then( ( data ) =>
+    try
+    {
+        let nextStartIndex = 0;
+        let likes = [ ];
+        while( nextStartIndex !== -1 )
         {
-            return response.status( 200 ).send( data );
-        } )
-        .catch( ( error ) =>
-        {
-            let statusCode = error && error.statusCode || 500;
-            let message = error && error.message || 'An error occured.';
-            return response.status( statusCode ).send( message );
-        } );
+            console.log( 'Station Likes - Next Start Index: ' + nextStartIndex );
+            let result = await getStationLikes( stationId, nextStartIndex );
+            likes.push( ...result.likes );
+            nextStartIndex = result.nextStartIndex;
+        }
+        return response.status( 200 ).send( likes );
+    }
+    catch( error )
+    {
+        let statusCode = error && error.statusCode || 500;
+        let message = error && error.message || 'An error occured.';
+        return response.status( statusCode ).send( message );
+    }
 } );
 
 export default stationsLikes;
+
+function getStationLikes( stationId: string, index: number )
+{
+    let query = {
+        stationId: stationId,
+        posFeedbackStartIndex: index,
+        posSortAsc: false,
+        posSortBy: 'date'
+    };
+    let newUrl = url.format( { protocol: 'https', host: 'www.pandora.com', pathname: '/content/station_track_thumbs/', query: query } );
+    return rp( newUrl )
+        .then( ( data ) =>
+        {
+            let $ = cheerio.load( data );
+            let likesList = $( 'li h3' ).map( ( index, element ) => {
+                let children = $( element ).children();
+                return {
+                    trackName: children.first().text(),
+                    artist: children.last().text()
+                };
+            } ).get();
+
+            if( $( '.no_more' ).length > 0 )
+            {
+                return {
+                    nextStartIndex: -1,
+                    likes: likesList
+                };
+            }
+            else
+            {
+                let nextStartIndex = $( '.show_more' ).data( 'nextstartindex' );
+
+                return {
+                    nextStartIndex: nextStartIndex,
+                    likes: likesList
+                };
+            }
+        } );
+}
--- src/routes/stations.ts
+++ src/routes/stations.ts
@@ -5,36 +5,40 @@ import * as cheerio from 'cheerio';
 
 const stations = Router();
 
-stations.get( '/:username', ( request, response, next ) =>
+stations.get( '/:username', async ( request, response, next ) =>
 {
     let username = request.params.username;
     console.log( 'Stations: ' + username );
 
-    let newUrl = url.format( { protocol: 'https', host: 'www.pandora.com', pathname: '/content/stations/', query: { startIndex: 0, webname: username } } );
+    let query = {
+        startIndex: 0,
+        webname: username
+    };
+    let newUrl = url.format( { protocol: 'https', host: 'www.pandora.com', pathname: '/content/stations/', query: query } );
 
     let jar = rp.jar();
-    jar.setCookie( rp.cookie( 'at=woLHZimYH3wf/Pu/U8eUM/XcMXanerCMMxiDXmZEjLvZgUyAZliC+l8Yg15mRIy72DlxLzLZ5Oe6+aO3CSzeYvw==' ), newUrl );
+    jar.setCookie( rp.cookie( 'at=wOV69NG5MjdXetjSbEwrXhp+3DRT9Sa1F3StkVPUa4yBJbkAH4vE924LdiJQ/7LOf9sZTl5G6AJMi5NAHE38hsSrAzjOI3aDwY2RBbkFtbBJmTZAJloA1qqhbL9o2CwrUvmjtwks3mL8=' ), newUrl );
 
-    return rp( { url: newUrl, jar: jar } )
-        .then( ( data ) =>
-        {
-            let $ = cheerio.load( data );
-            let stationsList = $( '#stations_container h3 span.artist_name a' ).map( ( index, element ) => {
-                let cElement = $( element );
-                return {
-                    stationId: cElement.attr( 'href' ).replace( '/station/', '' ),
-                    stationName: cElement.text().replace( / Radio$/, '' )
-                };
-            } ).get();
+    try
+    {
+        let htmlData = await rp( { url: newUrl, jar: jar } );
+        let $ = cheerio.load( htmlData );
+        let stationsList = $( '#stations_container h3 span.artist_name a' ).map( ( index, element ) => {
+            let cElement = $( element );
+            return {
+                stationId: cElement.attr( 'href' ).replace( '/station/', '' ),
+                stationName: cElement.text().replace( / Radio$/, '' )
+            };
+        } ).get();
 
-            return response.status( 200 ).json( stationsList );
-        } )
-        .catch( ( error ) =>
-        {
-            let statusCode = error && error.statusCode || 500;
-            let message = error && error.message || 'An error occured.';
-            return response.status( statusCode ).send( message );
-        } );
+        return response.status( 200 ).json( stationsList );
+    }
+    catch( error )
+    {
+        let statusCode = error && error.statusCode || 500;
+        let message = error && error.message || 'An error occured.';
+        return response.status( statusCode ).send( message );
+    }
 } );
 
 export default stations;
